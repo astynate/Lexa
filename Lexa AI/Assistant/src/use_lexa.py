fc53b8e17375071a67f34903fbfff6b3e9574c7a
unknown <sicome.a.s@gmail.com>
2024-03-10 23:55:02 +0300

    Use model

---
+++ Lexa AI/Assistant/src/use_lexa.py
@@ -0,0 +1,18 @@
+from __model__ import *
+
+BASE_PATH = 'D:/Exider Company/Lexa/Lexa AI/Assistant'
+lexa = Lexa(50, BASE_PATH + '/models/lexa_tokenizer.pickle', 1, path=BASE_PATH + '/models/lexa.keras')
+
+for i in range(5):
+
+    context = lexa.tokenizer.get_text([np.random.randint(1, 30)])
+
+    print(str(i))
+    print(context, end=' ')
+
+    for k in range(10):
+        
+        generated_word = lexa(context)
+        context += generated_word
+
+        print(generated_word)
