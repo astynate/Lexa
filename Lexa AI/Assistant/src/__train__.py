52e9dff576c84882254d9c63dc11261beff1cfc5
Святослав Андреев <sicome.a.s@gmail.com>
2024-03-12 18:10:03 +0300

    Check point

--- Lexa AI/Assistant/src/__train__.py
+++ Lexa AI/Assistant/src/__train__.py
@@ -12,31 +12,49 @@ def train() -> None:
     lexa = Lexa(50, BASE_PATH + '/models/lexa_tokenizer.pickle', 1, path=BASE_PATH + '/models/lexa.keras')
     text = open('D:/Exider Company/Lexa/Lexa AI/Assistant/dataset/original/book_one.txt', 'rb').read().decode(encoding='utf-8')
 
-    sequences = lexa.tokenizer.get_full_sequence(text)
-    sequences = np.array([sequences[i:i+51] for i in range(len(sequences)-50)])
+    full_sequence = lexa.tokenizer.get_full_sequence(text)
+    sequences = [full_sequence[i:i+np.random.randint(2, 51)] for i in range(len(full_sequence)-50)]
 
-    print(sequences)
+    input_values = []
+    correct_answers = []
 
-    train_data, train_labels = sequences[:, :-1], sequences[:, -1]
-    train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=lexa.tokenizer.get_dimension())
+    for sub_sequence in sequences:
 
-    print(train_data.shape)
-    print(train_labels.shape)
+        correct = lexa.tokenizer.get_text([sub_sequence[-1]])
 
-    for _ in range(1):
+        if correct != '\ufffd':
 
-        lexa.model.fit(train_data, train_labels, batch_size=64, epochs=1)
-        lexa.model.save(BASE_PATH + '/models/lexa.keras')
+            value = lexa.tokenizer.get_sequences(lexa.tokenizer.get_text(sub_sequence[:-1]))
+            token_probability = tf.keras.utils.to_categorical(sub_sequence[-1], num_classes=lexa.tokenizer.get_dimension())
 
-        context = lexa.tokenizer.get_text([np.random.randint(1, 30)])
-        print(context)
+            input_values.append(value)
+            correct_answers.append(token_probability)
 
-        for i in range(1):
+    print(np.array(input_values).shape)
+    print(np.array(correct_answers).shape)
+    print(np.array(input_values))
+
+    for i in input_values:
+
+        print(i)
+
+    # print(np.array(correct_answers))
+
+    # for _ in range(1):
+
+    #     lexa.model.fit(np.array(input_values), np.array(correct), batch_size=64, epochs=1)
+    #     lexa.model.save(BASE_PATH + '/models/lexa.keras')
+
+    #     context = lexa.tokenizer.get_text([np.random.randint(1, 30)])
+
+    #     print(context)
+
+    #     for i in range(1):
             
-            print(str(i) + ' |' + context + '|')
-            generated_word = lexa(context)
+    #         print(str(i) + ' |' + context + '|')
+    #         generated_word = lexa(context)
 
-            context += generated_word
+    #         context += generated_word
 
 if __name__ == '__main__':
 
--- Lexa AI/Assistant/src/tokenizer.py
+++ Lexa AI/Assistant/src/tokenizer.py
@@ -1,112 +1,64 @@
 import os
 import pickle
+import tensorflow_datasets as tfds
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 def load_dataset(path: str) -> list:
     with open(path, encoding='utf-8') as data:
         dataset = data.read().lower()
 
-    return dataset
+    return dataset.split()
 
 class LexaTokenizer:
 
     def __init__(self, path: str, **kwargs) -> None:
 
         if os.path.exists(path):
-
             with open(path, 'rb') as f:
-                self.dictionary = pickle.load(f)
-        
+                self.tokenizer = pickle.load(f)
         else:
+            
+            dataset = kwargs.get('dataset')
 
-            text = self.prepare(kwargs.get('text'))
-            self.dictionary = set()
+            if dataset is not None:
+                self.tokenizer = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+                    (text for text in dataset), target_vocab_size=2**13)
+                
+                with open(path, 'wb') as f:
+                    pickle.dump(self.tokenizer, f)
+            else:
+                raise ValueError("No existing tokenizer found and no dataset provided to train a new one.")
 
-            self.set_default(text)
-            self.add_pairs(text)
-            self.dictionary = list(self.dictionary)
-
-            with open(path, 'wb') as f:
-                pickle.dump(self.dictionary, f)
-
-        print(f'LexaTokenizer created, number of tokens in dictionary: {len(self.dictionary)}')
-
-    def prepare(self, text: str) -> str:
-        return text.lower()
-
-    def set_default(self, text) -> None:
-        """
-        Given a text, splits it into words and adds each symbol 
-        from each word to the dictionary.
-        """
-        for word in text.split():
-            self.dictionary.update(set(word))
-
-        self.dictionary.add(' ')
-
-    def add_pairs(self, text: str) -> None:
-
-        pairs = set()
-
-        to_replace = [',', ' ', '!', '?', '-', '\n', ')', '(' '[', ']', ':', "'"]
-        to_replace += [str(i) for i in range(9)]
-
-        for char in to_replace:
-            text = text.replace(char, '')
-
-        for i in range(len(text) - 1):
-            pair = text[i:i + 2]
-            pairs.add(pair)
-
-        for pair in pairs:
-            self.dictionary.add(pair)
 
     def get_full_sequence(self, text: str) -> list:
+        return self.tokenizer.encode(text)
 
-        text = text.lower().split()
-        sequence = []
+    def get_sequences(self, text: str) -> list:
+        sequences = self.tokenizer.encode(text)
+        return pad_sequences([sequences], maxlen=50, padding='post')[0]
 
-        for word in text:
-
-            start = 0
-            end = len(word) + 1
-
-            while start < end:
-
-                end -= 1
-                
-                if word[start:end] in self.dictionary:
-
-                    sequence.append(self.dictionary.index(word[start:end]))
-                    start = end
-                    end = len(word) + 1
-            
-            sequence.append(self.dictionary.index(' '))
-
-        return sequence
-    
     def get_text(self, sequences: list) -> str:
-        return ''.join(self.dictionary[i] for i in sequences)
+        return self.tokenizer.decode(sequences) 
     
     def get_dimension(self) -> int:
-        return len(self.dictionary)
-    
-    def get_sequences(self, text: str) -> list:
-        sequences = self.get_full_sequence(text)
-        
-        if len(sequences) > 50:
-            return sequences[:50]
-        elif len(sequences) < 50:
-            return sequences + [0] * (50 - len(sequences))
-        else:
-            return sequences
+        return self.tokenizer.vocab_size
 
 if __name__ == '__main__':
 
-    dataset = load_dataset('D:/Exider Company/Lexa/Lexa AI/Assistant/dataset/original/russian.txt')
-    tokenizer = LexaTokenizer('D:/Exider Company/Lexa/Lexa AI/Assistant/models/lexa_tokenizer.pickle' , text=dataset)
-    
-    tokens = tokenizer.get_full_sequence('привет')
-    original = tokenizer.get_text(tokens)
+    model_path: str = 'D:/Exider Company/Lexa/Lexa AI/Assistant/models/lexa_tokenizer.pickle'
+    dataset = load_dataset('D:/Exider Company/Lexa/Lexa AI/Assistant/dataset/original/book_one.txt')
 
-    print(tokens)
-    print(original)
+    tokenizer = LexaTokenizer(model_path, dataset=dataset)
+
+    test_token = tokenizer.get_sequences('привет как дела')
+    original_text = tokenizer.get_text(test_token)
+
+    for token in range(tokenizer.get_dimension()):
+        print(tokenizer.get_text([token]))
+
+    print(len(dataset))
+
+    print(test_token)
+    print(original_text)
+
+    print("Количество токенов в токенизаторе: ", tokenizer.tokenizer.vocab_size)
